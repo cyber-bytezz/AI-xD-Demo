3fb17b9cf5a9c24de94abdd16285bc928db38c44
Aro Barath Chandru B <chandru2021007@gmail.com>
2023-07-23 23:21:44 +0530

    Nav
    Auth , NavBar Added

---
+++ ai-xd/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button"
+import { UserButton } from "@clerk/nextjs";
+
+ const DashboardPage = () => {
+  return (
+   <div>
+     <p>Dashboard page (Protected)</p>
+      
+   </div>
+ 
+  )
+}
+
+export default DashboardPage;
---
+++ ai-xd/components/sidebar.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+const Sidebar = () => {
+    return (
+        <div className="space-y-4 py-4 flex flex-col h-full
+        bg-{#111827} text-white">
+            Helo Side Component
+         </div>       
+    );
+}
+
+export default Sidebar;
